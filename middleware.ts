d1783bc00684794acb7e0a872c5cb6208fc428c3
Sanjeev Jha <jhasavi@users.noreply.github.com>
2026-04-13 15:25:41 -0400

    Restore middleware with authMiddleware for Vercel deployment

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,8 @@
-// Temporarily disabled to debug production 500 error
-// import { authMiddleware } from '@clerk/nextjs/server'
+import { authMiddleware } from '@clerk/nextjs/server'
 
-// export default authMiddleware({
-//   publicRoutes: ['/', '/sign-in(.*)', '/sign-up(.*)', '/onboarding(.*)', '/api/webhooks(.*)'],
-// })
-
-export default function middleware(request: Request) {
-  return
-}
+export default authMiddleware({
+  publicRoutes: ['/', '/sign-in(.*)', '/sign-up(.*)', '/onboarding(.*)', '/api/webhooks(.*)'],
+})
 
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
